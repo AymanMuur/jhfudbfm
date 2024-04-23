099d84a76744cdcdb668b9c54ec76bed8e937002
Meow <murzagalieva_1960@mail.ru>
2024-04-23 11:19:18 +0300

    added HW

---
+++ Desktop/GeekBrains/3_Znakomstvo_s_yazykami_programmirovaniya/3_23.04.2024/HomeWork/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая принимает 
+// на вход число и проверяет, кратно ли оно 
+// одновременно 7 и 23
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt16(Console.ReadLine());
+
+int dif1 = num % 7;
+int dif2 = num % 23;
+
+if (dif1 == 0 && dif2 == 0)
+{
+    Console.Write("Да!");
+}
+else
+{
+    Console.Write("Нет!");
+}
+
+
+
---
+++ Desktop/GeekBrains/3_Znakomstvo_s_yazykami_programmirovaniya/3_23.04.2024/HomeWork/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4: Напишите программу, которая на вход 
+// принимает натуральное число N, а на выходе 
+// показывает его цифры через запятую
+
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 10)
+{
+    Console.WriteLine(num);
+}
+else
+{
+    while (num > 0)
+    {
+        int currentDigit = num % 10;
+        num /= 10;
+        if (num > 0)
+        {
+            Console.Write(currentDigit + ",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
+    }
+}
+
