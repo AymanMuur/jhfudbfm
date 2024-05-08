59564b357c7ff7417028c0b6e2e96df81817c5ae
Meow <murzagalieva_1960@mail.ru>
2024-05-08 13:57:23 +0300

    add Lesson2:Task1

---
+++ Desktop/test/Lesson2/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 3;
+arr[2] = 6;
+arr[3] = 1;
+arr[4] = 8;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+// Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5] { 3, 5, 1, 7, 9 };
+int[] arr3 = { 3, 5, 7, 9, 2 }; // arr3 = {3, 5, 7, 9, 2};
+
