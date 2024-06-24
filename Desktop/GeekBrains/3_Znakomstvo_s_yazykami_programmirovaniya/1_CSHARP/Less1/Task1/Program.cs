4f9dc1cfda49e9b3d412aa50ff70199d2676cc27
Meow <murzagalieva_1960@mail.ru>
2024-06-24 15:30:30 +0300

    add Lesson1:Task1

---
+++ Desktop/GeekBrains/3_Znakomstvo_s_yazykami_programmirovaniya/1_CSHARP/Less1/Task1/Program.cs
@@ -0,0 +1,4 @@
+﻿// .gitignore, создайте файл(.gitignore) у себя и скопируйте в него содержимое по данной ссылке 
+// https://github.com/sugrevinVitalii/gitignore_iksergey/blob/main/gitignore.txt
+
+Console.WriteLine("Hello, World!");
