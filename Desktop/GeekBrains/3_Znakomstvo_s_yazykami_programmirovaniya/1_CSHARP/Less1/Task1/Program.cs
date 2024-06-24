53f6a4832be982eaaab22c27fe6ed96217d3f7c3
Meow <murzagalieva_1960@mail.ru>
2024-06-24 16:03:51 +0300

    add some variables: Lesson1:Task1

--- Desktop/GeekBrains/3_Znakomstvo_s_yazykami_programmirovaniya/1_CSHARP/Less1/Task1/Program.cs
+++ Desktop/GeekBrains/3_Znakomstvo_s_yazykami_programmirovaniya/1_CSHARP/Less1/Task1/Program.cs
@@ -1,4 +1,14 @@
 ﻿// .gitignore, создайте файл(.gitignore) у себя и скопируйте в него содержимое по данной ссылке 
 // https://github.com/sugrevinVitalii/gitignore_iksergey/blob/main/gitignore.txt
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+System.Console.WriteLine(d);
+System.Console.WriteLine(c);
+System.Console.WriteLine(b);
