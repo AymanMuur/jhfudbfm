6ca4fe4284d06fda564a14b4dbeed01d34926415
Meow <murzagalieva_1960@mail.ru>
2024-06-24 18:12:57 +0300

    add Lesson1:Task4

---
+++ Desktop/GeekBrains/3_Znakomstvo_s_yazykami_programmirovaniya/1_CSHARP/Less1/Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4.
+// Дано: 5 гирь с разным весом в случайном порядке.
+// Найти: вес самой тяжёлой гири.
+
+int a = 95;
+int b = 66;
+int c = 2;
+int d = 800;
+int e = 34;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
+
