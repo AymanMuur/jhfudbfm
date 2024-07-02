8d7c0ed174aaa3a0bd71f43f6bc6fd401ac35e2a
Meow <murzagalieva_1960@mail.ru>
2024-07-02 14:06:23 +0300

    add task1:lesson2

---
+++ Desktop/GeekBrains/3_Znakomstvo_s_yazykami_programmirovaniya/1_CSHARP/Less2/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 4;
+arr[2] = 7;
+arr[3] = 9;
+arr[4] = 1;
+
+Console.Write($"{arr[0]}, ");
+Console.Write($"{arr[1]}, ");
+Console.Write($"{arr[2]}, ");
+Console.Write($"{arr[3]}, ");
+Console.Write($"{arr[4]}");
+
+// Console.Write(arr[5]);
+
+int[] arr2 = new int[5] { 3, 4, 7, 0, 6 };
+int[] arr3 = { 3, 4, 7, 0, 6 };
+// arr = [1, 2, 3, 4, 5] - массив на питоне
+
+
