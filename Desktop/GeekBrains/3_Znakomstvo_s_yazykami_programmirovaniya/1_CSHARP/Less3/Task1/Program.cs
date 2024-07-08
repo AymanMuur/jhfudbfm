9cf2c7d55041d62e0340e28686a608e2249c44b7
Meow <murzagalieva_1960@mail.ru>
2024-07-08 19:51:53 +0300

    add Less3:Task1

---
+++ Desktop/GeekBrains/3_Znakomstvo_s_yazykami_programmirovaniya/1_CSHARP/Less3/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вычислить значение формулы: a*b/c+d, 
+// где  a,b,c,d - некоторые целые числа.
+// Решение оформить в виде функции. 
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+
+    double result = numenator / denumenator;
+    return result;
+}
+
+double res = CalculateFormula(9, 1, 3, 5);
+Console.WriteLine(res);
